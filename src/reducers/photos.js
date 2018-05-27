56728a9a3329ee6090071d357a2a6537239879b2
Vanessa Van <vanessadorbs@users.noreply.github.com>
2018-05-26 19:15:35 -0500

    webpanel, localstorage

--- src/reducers/photos.js
+++ src/reducers/photos.js
@@ -2,34 +2,43 @@ import { PREVIOUS_PHOTO, NEXT_PHOTO, LATEST_PHOTO } from 'constants/ActionTypes'
 
 import list from '../photos.json'
 
-const initialState = {
-  list,
-  index: 0
-}
-
 export function wrap(index, length) {
   if (index < 0) return index + length
   if (index >= length) return index - length
   return index
 }
 
+const initialState = {
+  list,
+  index: wrap(window.localStorage.index - 0, list.length)
+}
+
 export default function photosReducer(state = initialState, action) {
   switch (action.type) {
-    case PREVIOUS_PHOTO:
+    case PREVIOUS_PHOTO: {
+      const index = wrap(state.index - 1, state.list.length)
+      window.localStorage.index = index
       return {
         ...state,
-        index: wrap(state.index - 1, state.list.length)
+        index
       }
-    case NEXT_PHOTO:
+    }
+    case NEXT_PHOTO: {
+      const index = wrap(state.index + 1, state.list.length)
+      window.localStorage.index = index
       return {
         ...state,
-        index: wrap(state.index + 1, state.list.length)
+        index
       }
-    case LATEST_PHOTO:
+    }
+    case LATEST_PHOTO: {
+      const index = state.list.length - 1
+      window.localStorage.index = index
       return {
         ...state,
-        index: state.list.length - 1
+        index
       }
+    }
     default:
       return state
   }
